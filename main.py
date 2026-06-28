b676180832425aab527ca9c387194cddf87496f6
Ayushh10 <aayush2sm@gmail.com>
2026-06-28 14:50:53 +0530

    Commit V3: POST

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-from fastapi import FastAPI
+from random import randint
+from fastapi import FastAPI, HTTPException, Request
 from datetime import datetime
-
-app = FastAPI(root_path="/api/v1/")
+from typing import Any
+# app = FastAPI(root_path="/api/v1/")
+app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
 
-# def class Campaigns(self):
-#     campaign_id = int
-#     campaign_name = str
-#     date_created = datetime.now()
-#     last_updated = datetime.now()
-
-    
-
-Data = Any: [
+data : Any = [
     {
-        campaign_id = int
-        campaign_name = str
-        date_created = datetime.now()
-        last_updated = datetime.now()},
-    {}
+        "campaign_id": 1,
+        "campaign_name": "Summer Launch",
+        "date_created": datetime.now(),
+        "last_updated": datetime.now()
+        },
+    {
+        "campaign_id": 2,
+        "campaign_name": "Winter Launch",
+        "date_created": datetime.now(),
+        "last_updated": datetime.now()
+        }
 ]
+
 @app.get("/campaigns")
 async def campaigns():
-    return {"campaings": "Campaign-name"}
+    return {"campaings": data}
+
+@app.get("/campaigns/{id}")
+async def read_campaigns(id: int):
+    for campaign in data:
+        if campaign.get("campaign_id") == id:
+            return campaign
+    raise HTTPException(status_code=404) 
 
 @app.post("/campaigns")
-async def add_new("campaigns": input()):
-    return 
+async def create_camp(reqeust: Request):
+    body = await reqeust.json()
+
+    new : Any = {
+        "campaign_id": randint(10,100),
+        "campaign_name": body.get("name"),
+        "date_created": body.get("date_created"),
+        "last_updated": datetime.now()
+    }
+    data.append(new)
+    return {"data added": new}
+
